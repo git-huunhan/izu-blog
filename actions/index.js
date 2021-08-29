dd0aee0f07031a98b703484cd9ba2b717d3640b4
Huu Nhan <huunhankirigamer@gmail.com>
2021-08-29 14:55:11 +0700

    Fixed actions bug with luv :heart:

--- actions/index.js
+++ actions/index.js
@@ -2,6 +2,10 @@ import useSWR from "swr";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
+export const getBlogs = (url) => {
+  return fetcher(url);
+};
+
 export const useGetBlogs = (initialData) => {
   return useSWR(`/api/blogs`, fetcher, { initialData });
 };
